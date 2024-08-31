7bd9933fd1357da8f958fd5122b6906316196479
kacpoks1 <abramowiczkacper66@gmail.com>
2024-08-31 22:35:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.fuelcalculator
+
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+
+
+class MainActivity : ComponentActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+
+        val editTextDate = findViewById<EditText>(R.id.date)
+
+        val currentDate = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Calendar.getInstance().time)
+
+        editTextDate.setText(currentDate)
+
+        val graphView: GraphView = findViewById(R.id.idGraphView)
+
+        // Dodawanie danych do GraphView
+        val series = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 20.0),
+                DataPoint(1.0, 3.0),
+                DataPoint(2.0, 4.0),
+                DataPoint(3.0, 10.0),
+                DataPoint(4.0, 6.0),
+                DataPoint(5.0, 0.0),
+                DataPoint(6.0, 6.0),
+                DataPoint(7.0, 1.0),
+                DataPoint(8.0, 2.0)
+            )
+        )
+
+        // Ustawianie tytułu
+        graphView.title = "Spalanie samochodu"
+
+
+        // Ustawianie rozmiaru tekstu tytułu
+        graphView.titleTextSize = 24f
+
+        // Dodawanie serii danych do GraphView
+        graphView.addSeries(series)
+    }
+
+}
+
