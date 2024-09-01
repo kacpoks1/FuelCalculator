b0e16a43c58f5a9a57e7a8a27a30d493a5d864c6
kacpoks1 <abramowiczkacper66@gmail.com>
2024-09-01 22:03:05 +0200

    full working with out graph

--- app/src/main/java/com/example/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fuelcalculator
 
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.EditText
 import androidx.activity.ComponentActivity
@@ -26,6 +27,67 @@ class MainActivity : ComponentActivity() {
         setContentView(binding.root)
         enableEdgeToEdge()
 
+        // Pobierz SharedPreferences
+        val sharedPref = getSharedPreferences("Car", Context.MODE_PRIVATE)
+
+        // Załaduj zapisane wartości i wyświetl je na starcie
+        val totalKilometers = sharedPref.getInt("km", 0)
+        val totalValue = sharedPref.getInt("money", 0)
+        val totalFuel = sharedPref.getInt("liters", 0)
+        val totalTrip = sharedPref.getInt("trip", 0)
+
+        // Oblicz spalanie na 100 km
+        if (totalKilometers > 0) {
+            val fuelConsumption = (totalFuel.toDouble() / totalKilometers.toDouble()) * 100
+            val roundedFuelConsumption = "%.2f".format(fuelConsumption)
+            binding.spalanieDane.setText("Spalanie: $roundedFuelConsumption l/100km")
+        } else {
+            binding.spalanieDane.setText("Enter valid kilometers")
+        }
+        binding.kosztCalkowityDane.setText("$totalValue zł")
+        binding.odlegloscDane.setText("$totalKilometers km")
+
+
+
+        // Reszta kodu, np. obsługa przycisków itd.
+        binding.confirm.setOnClickListener {
+            val prevKilometers = sharedPref.getInt("km", 0)
+            val prevValue = sharedPref.getInt("money", 0)
+            val prevFuel = sharedPref.getInt("liters", 0)
+
+            val newKilometers = binding.kilomiters.text.toString().toInt()
+            val newValue = binding.value.text.toString().toInt()
+            val newFuel = binding.fuel.text.toString().toInt()
+            val date = binding.date.text.toString()
+
+            val totalKilometers = prevKilometers + newKilometers
+            val totalValue = prevValue + newValue
+            val totalFuel = prevFuel + newFuel
+            val totalTrip =  prevKilometers + newKilometers
+
+
+
+            val editor = sharedPref.edit()
+            editor.apply {
+                putInt("km", totalKilometers)
+                putInt("money", totalValue)
+                putInt("liters", totalFuel)
+                putString("day", date)
+                apply()
+            }
+
+            
+            binding.kosztCalkowityDane.setText("$totalValue zł")
+            binding.odlegloscDane.setText("$totalKilometers km")
+
+            if (totalKilometers > 0) {
+                val fuelConsumption = (totalFuel.toDouble() / totalKilometers.toDouble()) * 100
+                val roundedFuelConsumption = "%.2f".format(fuelConsumption)
+                binding.spalanieDane.setText("Spalanie: $roundedFuelConsumption l/100km")
+            } else {
+                binding.spalanieDane.setText("Enter valid kilometers")
+            }
+        }
 
 
 
